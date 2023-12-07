cb80fbcb73f24201b12eeb2abdf8dc60d665040f
RaphaelNY <1278173058@qq.com>
2023-12-07 21:30:35 +0800

    compelte head file

---
+++ Schoolguide/Schoolguide/Pointer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+using namespace std;
+
+struct PathNode {
+    int vA;
+    int vB;
+    int pathlen;
+    PathNode* next;
+
+    PathNode(int vA, int vB, int pathlen) : vA(vA), vB(vB), pathlen(pathlen), next(nullptr) {}
+};
+
+struct Pointinfo {
+    string name;
+    string info;
+    int num;
+    Pointinfo* next;
+    Pointinfo(string name, string info, int num) : name(name), info(info), num(num), next(nullptr) {}
+};
+
+struct Pathinfo {
+    int length;
+    int* Path;
+    int PathLength;
+};
+
+void buildingAdjoint_matrices(int numbers, PathNode* head);
+
+int addPathNode(PathNode* head, int vA, int vB, int pathlen, int numbers);
+int popPathNode(PathNode* head, int numbers);
+void print_allPathNodes(PathNode* head);
+
+int addPointinfo(Pointinfo* head, string name, string info, int num, int nums);
+int popPointinfo(Pointinfo* head, int nums);
+void print_allPointinfo(Pointinfo* head);
+
+
+Pathinfo** Floyd_A(int number);
+int** Floyd_B(int numbers);
+void printPath(Pathinfo** paths, int start, int end);
+// int* Dijkstra(int numbers, int start, int end);
+
+void searchinfo(Pointinfo* head, int nums);
