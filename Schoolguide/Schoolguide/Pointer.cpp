1f32b8b87978b62111266e6d4641b7c4aa6aec64
RaphaelNY <1278173058@qq.com>
2023-12-08 12:15:40 +0800

    add function of 2&3

--- Schoolguide/Schoolguide/Pointer.cpp
+++ Schoolguide/Schoolguide/Pointer.cpp
@@ -283,3 +283,126 @@ void searchinfo(Pointinfo* head, int nums) {
 	cout << endl;
 	return distance;
 }*/
+
+void DFS(int u, int* disc, int* low, int* parent, bool* ap, int& time, int numbers) {
+	int children = 0;
+	disc[u] = low[u] = ++time;
+
+	for (int v = 0; v < numbers; ++v) {
+		if (Adjacency_matrix[u][v] != 0) {
+			if (disc[v] == -1) {
+				children++;
+				parent[v] = u;
+				DFS(v, disc, low, parent, ap, time, numbers);
+
+				low[u] = min(low[u], low[v]);
+
+				if (parent[u] == -1 && children > 1)
+					ap[u] = true;
+
+				if (parent[u] != -1 && low[v] >= disc[u])
+					ap[u] = true;
+			}
+			else if (v != parent[u]) {
+				low[u] = min(low[u], disc[v]);
+			}
+		}
+	}
+}
+
+void DFS(int start, int end, int numbers, int* path, bool* visited, int pathLength) {
+	path[pathLength++] = start;
+	visited[start] = true;
+
+	if (start == end) {
+		cout << "Path: ";
+		for (int i = 0; i < pathLength; ++i) {
+			cout << path[i] << " ";
+		}
+		cout << endl;
+	}
+	else {
+		for (int i = 0; i < numbers; ++i) {
+			if (Adjacency_matrix[start][i] != 0 && !visited[i]) {
+				DFS(i, end, numbers, path, visited, pathLength);
+			}
+		}
+	}
+	visited[start] = false;
+}
+
+void findArticulationPoints(int numbers) {
+	int* disc = new int[numbers];
+	int* low = new int[numbers];
+	int* parent = new int[numbers];
+	bool* ap = new bool[numbers];
+	int time = 0;
+
+	for (int i = 0; i < numbers; i++) {
+		disc[i] = -1;
+		low[i] = -1;
+		parent[i] = -1;
+		ap[i] = false;
+	}
+
+	for (int i = 0; i < numbers; i++) {
+		if (disc[i] == -1) {
+			DFS(i, disc, low, parent, ap, time, numbers);
+		}
+	}
+
+	cout << "key point is:";
+	int num = 0;
+	for (int i = 0; i < numbers; i++) {
+		if (ap[i]) {
+			cout << i << " ";
+			num++;
+		}
+	}
+	if(num== 0)
+		cout << "no key point" << endl;
+	cout << endl;
+
+	delete[] disc;
+	delete[] low;
+	delete[] parent;
+	delete[] ap;
+}
+
+void traverseAllPaths(int start, int end, int numbers) {
+	bool* visited = new bool[numbers] {false};
+	int* path = new int[numbers];
+	int pathLength = 0;
+
+	DFS(start, end, numbers, path, visited, pathLength);
+
+	delete[] visited;
+	delete[] path;
+}
+
+void findShortestPathWithVia(Pathinfo** paths, int numbers, int start, int end, int via) {
+	for (int i = 0; i < numbers; i++) {
+		int viaIndex = -1;
+		for (int i = 0; i < numbers; i++) {
+			if (i != start && i != end && i == via) {
+				viaIndex = i;
+				break;
+			}
+		}
+
+		if (viaIndex == -1) {
+			cerr << "Error: The specified 'via' node does not exist or is the same as start/end nodes." << endl;
+			return;
+		}
+
+		cout << "Shortest Path from " << start << " to " << end << " via " << via << ":" << endl;
+		cout << "Length: " << paths[start][viaIndex].length + paths[viaIndex][end].length << " - ";
+		for (int i = 0; i < paths[start][viaIndex].pathLength; i++) {
+			cout << paths[start][viaIndex].path[i] << " ";
+		}
+		for (int i = 1; i < paths[viaIndex][end].pathLength; i++) {
+			cout << paths[viaIndex][end].path[i] << " ";
+		}
+		cout << endl;
+	}
+}
