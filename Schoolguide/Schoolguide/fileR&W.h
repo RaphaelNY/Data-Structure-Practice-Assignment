d74ae3a9c64eb328e90fa9be3c1c16486911c22b
RaphaelNY <1278173058@qq.com>
2023-12-08 10:36:25 +0800

    update

--- Schoolguide/Schoolguide/fileR&W.h
+++ Schoolguide/Schoolguide/fileR&W.h
@@ -11,6 +11,7 @@ int AddingPointinfo(Pointinfo* head, int nums, int nu);
 
 int PopingPointinfo(Pointinfo* head, string name, int nums);
 
-int Rawdata_readinfromFile(PathNode* head, string readfilePath);
+int Rawdata_readinfromFile(PathNode* head, string readfilePath_1);
+int Rawdata_readinfromFile(Pointinfo* infohead, string readfilePath_2);
 
-void Rawdata_saveinFile(PathNode* head, Pointinfo* infohead, int nums, int numbers, string savefilePath);
+void Rawdata_saveinFile(PathNode* head, Pointinfo* infohead, int nums, int numbers, string savefilePath_1, string savefilePath_2);
